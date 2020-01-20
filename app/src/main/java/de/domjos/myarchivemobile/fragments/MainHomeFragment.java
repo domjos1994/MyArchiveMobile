f13616be39e8c0fe5f79d692869ff1e29f83fa44
domjos1994 <developing@domjos.de>
2020-01-20 15:16:34 +0100

    solve bug 0000463

--- app/src/main/java/de/domjos/myarchivemobile/fragments/MainHomeFragment.java
+++ app/src/main/java/de/domjos/myarchivemobile/fragments/MainHomeFragment.java
@@ -6,11 +6,23 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
 
 import de.domjos.customwidgets.model.objects.BaseDescriptionObject;
 import de.domjos.customwidgets.utils.MessageHelper;
 import de.domjos.customwidgets.widgets.swiperefreshdeletelist.SwipeRefreshDeleteList;
+import de.domjos.myarchivelibrary.model.general.Person;
 import de.domjos.myarchivelibrary.model.media.BaseMediaObject;
+import de.domjos.myarchivelibrary.model.media.LibraryObject;
+import de.domjos.myarchivelibrary.model.media.books.Book;
+import de.domjos.myarchivelibrary.model.media.games.Game;
+import de.domjos.myarchivelibrary.model.media.movies.Movie;
+import de.domjos.myarchivelibrary.model.media.music.Album;
 import de.domjos.myarchivemobile.R;
 import de.domjos.myarchivemobile.activities.MainActivity;
 import de.domjos.myarchivemobile.helper.ControlsHelper;
@@ -22,6 +34,34 @@ public class MainHomeFragment extends ParentFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.main_fragment_home, container, false);
         this.lvMedia = root.findViewById(R.id.lvMedia);
+
+        this.lvMedia.addButtonClick(R.drawable.ic_local_library_black_24dp, new SwipeRefreshDeleteList.ButtonClickListener() {
+            @Override
+            public void onClick(List<BaseDescriptionObject> objectList) {
+                try {
+                    AlertDialog.Builder b = new AlertDialog.Builder(Objects.requireNonNull(getActivity()));
+                    b.setTitle(getString(R.string.media_persons));
+                    List<String> personsString = new LinkedList<>();
+                    List<Person> people = MainActivity.GLOBALS.getDatabase().getPersons("", 0);
+                    for(Person person : people) {
+                        personsString.add(String.format("%s %s", person.getFirstName(), person.getLastName()).trim());
+                    }
+                    String[] types = personsString.toArray(new String[0]);
+                    b.setItems(types, (dialogInterface, i) -> {
+                        for(BaseDescriptionObject baseDescriptionObject : objectList) {
+                            LibraryObject libraryObject = new LibraryObject();
+                            libraryObject.setDeadLine(new Date());
+                            libraryObject.setNumberOfDays(7);
+                            libraryObject.setPerson(people.get(i));
+                            MainActivity.GLOBALS.getDatabase().insertOrUpdateLibraryObject(libraryObject, (BaseMediaObject) baseDescriptionObject.getObject());
+                        }
+                    });
+                    b.show();
+                } catch (Exception ex) {
+                    MessageHelper.printException(ex, R.mipmap.ic_launcher_round, getContext());
+                }
+            }
+        });
 
         this.lvMedia.reload(new SwipeRefreshDeleteList.ReloadListener() {
             @Override
@@ -37,6 +77,26 @@ public class MainHomeFragment extends ParentFragment {
                 if(mainActivity != null) {
                     mainActivity.selectTab(listObject.getDescription(), ((BaseMediaObject) listObject.getObject()).getId());
                 }
+            }
+        });
+
+        this.lvMedia.deleteItem(new SwipeRefreshDeleteList.DeleteListener() {
+            @Override
+            public void onDelete(BaseDescriptionObject listObject) {
+                BaseMediaObject baseMediaObject = (BaseMediaObject) listObject.getObject();
+                if(baseMediaObject instanceof Book) {
+                    MainActivity.GLOBALS.getDatabase().deleteItem((Book) baseMediaObject);
+                }
+                if(baseMediaObject instanceof Movie) {
+                    MainActivity.GLOBALS.getDatabase().deleteItem((Movie) baseMediaObject);
+                }
+                if(baseMediaObject instanceof Game) {
+                    MainActivity.GLOBALS.getDatabase().deleteItem((Game) baseMediaObject);
+                }
+                if(baseMediaObject instanceof Album) {
+                    MainActivity.GLOBALS.getDatabase().deleteItem((Album) baseMediaObject);
+                }
+                reload();
             }
         });
 
