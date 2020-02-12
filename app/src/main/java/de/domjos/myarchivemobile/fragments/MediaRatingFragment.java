74e01f90ffaae07ebac3fea9f130e7eeb4064f7f
domjos1994 <developing@domjos.de>
2020-02-12 10:59:58 +0100

    solve bug 0000490

--- app/src/main/java/de/domjos/myarchivemobile/fragments/MediaRatingFragment.java
+++ app/src/main/java/de/domjos/myarchivemobile/fragments/MediaRatingFragment.java
@@ -7,16 +7,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import de.domjos.customwidgets.utils.Converter;
 import de.domjos.customwidgets.utils.Validator;
 import de.domjos.myarchivelibrary.model.media.BaseMediaObject;
 import de.domjos.myarchivemobile.R;
 
 public class MediaRatingFragment extends AbstractFragment<BaseMediaObject> {
     private RatingBar rbRatingOwn, rbRatingWeb;
+    private TextView lblRatingOwn, lblRatingWeb;
     private EditText txtRatingNote;
 
     private BaseMediaObject baseMediaObject;
@@ -30,8 +33,13 @@ public class MediaRatingFragment extends AbstractFragment<BaseMediaObject> {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         this.rbRatingOwn = view.findViewById(R.id.rbRatingOwn);
+        this.lblRatingOwn = view.findViewById(R.id.lblRatingOwn);
         this.rbRatingWeb = view.findViewById(R.id.rbRatingWeb);
+        this.lblRatingWeb = view.findViewById(R.id.lblRatingWeb);
         this.txtRatingNote = view.findViewById(R.id.txtRatingNote);
+
+        this.rbRatingOwn.setOnRatingBarChangeListener((ratingBar, v, b) -> this.lblRatingOwn.setText(Converter.convertDoubleToString(v * 10.0)));
+        this.rbRatingWeb.setOnRatingBarChangeListener((ratingBar, v, b) -> this.lblRatingWeb.setText(Converter.convertDoubleToString(v * 10.0)));
 
         this.changeMode(false);
     }
